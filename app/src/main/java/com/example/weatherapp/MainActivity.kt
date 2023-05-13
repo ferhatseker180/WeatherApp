f2be98d1914e47fe783276505c6033eaf65f9a39
ferhatseker180 <ferhatseker180@gmail.com>
2023-05-14 01:21:56 +0300

    Temel izin ayarları yapıldı.

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.weatherapp
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
+import android.net.Uri
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(this@MainActivity, "Your Location Provider Is Turned OFF. Please Turn It", Toast.LENGTH_LONG).
+            show()
+            val intent = startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        } else {
+            dexterValidatePermission()
+        }
+
+
+    }
+
+    private fun isLocationEnabled() : Boolean {
+
+        val locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(
+            LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val mLocationRequest = com.google.android.gms.location.LocationRequest()
+        mLocationRequest.priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest,mLocationCallback,
+            Looper.myLooper()
+        )
+
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation : Location? = locationResult.lastLocation
+            val latitude = mLastLocation?.latitude
+            Log.i("Current Latitude","$latitude")
+            val longitude = mLastLocation?.longitude
+            Log.i("Current Longitude","$longitude")
+
+        }
+    }
+
+    private fun dexterValidatePermission() {
+        Dexter.withActivity(this@MainActivity).withPermissions(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+
+                    requestLocationData()
+                }
+
+                if (report.isAnyPermissionPermanentlyDenied) {
+                    Toast.makeText(this@MainActivity,
+                        "You Have Denied Location Permission. Please Enable Them As It Mandatory For The App To Work.",
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermission()
+            }
+
+        }).onSameThread()
+            .check()
+    }
+
+    private fun showRationalDialogForPermission() {
+        AlertDialog.Builder(this@MainActivity).setMessage("It Looks Like You Have Turned Off Permissions Required For This Feature. It Can Be Enabled Under Application Settings.")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ){_,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package",packageName,null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e : ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("CANCEL"){dialog,
+            _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+}
