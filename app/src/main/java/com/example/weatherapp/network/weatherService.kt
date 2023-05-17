02e1deafafd5299ccf08447487e5fcd7a0a306b9
ferhatseker180 <ferhatseker180@gmail.com>
2023-05-17 16:52:41 +0300

    Temel izin ayarları yapıldı.Data Classlar temel seviyede oluşturuldu. Api ayarları yapıldı.

---
+++ app/src/main/java/com/example/weatherapp/network/weatherService.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.network
+
+import com.example.weatherapp.models.weatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface weatherService {
+    @GET("2.5/weather")
+    fun getWeather(
+        @Query("lon") lon : Double,
+        @Query("lat") lat : Double,
+        @Query("units") units: String?,
+        @Query("appid") appid: String?
+    ) : Call<weatherResponse>
+}
